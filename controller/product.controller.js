d8c858fd36205339678376beb0d22318a1c6644b
shariar17e <shariarhossain23@gmail.com>
2023-01-24 19:26:46 +0600

    handling error

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,9 +1,18 @@
-const { fetchProductServices, postProductService, fetchProductServicesById } = require("../services/product.services");
+const {
+  fetchProductServices,
+  postProductService,
+  fetchProductServicesById,
+} = require("../services/product.services");
 
 module.exports.fetchProducts = async (req, res) => {
   try {
     const product = await fetchProductServices();
-
+    if(product[0].length === 0){
+      res.status(400).json({
+        status:false,
+        message:"can't find data"
+      })
+    }
     res.status(200).json({
       status: true,
       message: "data get success",
@@ -18,47 +27,45 @@ module.exports.fetchProducts = async (req, res) => {
   }
 };
 
+module.exports.postProduct = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const result = await postProductService(data);
 
-
-module.exports.postProduct = async (req,res,next) => {
-    try {
-        const data = req.body;
-        const result = await postProductService(data);
-
-        res.status(200).json({
-            status:true,
-            message:"post success",
-            data:result
-        })
-    } catch (error) {
-        res.status(400).json({
-            status:false,
-            message:"can't create data",
-            error:error
-        })
+    res.status(200).json({
+      status: true,
+      message: "post success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      message: "can't create data",
+      error: error,
+    });
+  }
+};
+module.exports.fetchProductById = async (req, res, next) => {
+  try {
+    const result = await fetchProductServicesById(req.params.id);
+    if (result[0].length === 0) {
+      return res.status(400).json({
+        status: false,
+        message: "can't find data",
+      });
     }
-}
-module.exports.fetchProductById = async (req,res,next) => {
-    try {
-        const result = await fetchProductServicesById(req.params.id);
-      if(result[0].length === 0){
-        return res.status(400).json({
-          status:false,
-          message:"can't find data"
-        })
-      }
 
-      console.log(result);
-        res.status(200).json({
-            status:true,
-            message:"post success",
-            data:result[0][0]
-        })
-    } catch (error) {
-        res.status(400).json({
-            status:false,
-            message:"can't create data",
-            error:error
-        })
-    }
-}
+    console.log(result);
+    res.status(200).json({
+      status: true,
+      message: "post success",
+      data: result[0][0],
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      message: "can't create data",
+      error: error,
+    });
+  }
+};
